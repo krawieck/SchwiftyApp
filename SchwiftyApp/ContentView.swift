b72c3f5e93aeffb7ed00bfd61a50ae4de1a59800
Filip Krawczyk <fil.krawczyk@gmail.com>
2026-06-16 22:42:21 +0200

    Initial Commit

---
+++ SchwiftyApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SchwiftyApp
+//
+//  Created by Filip Krawczyk on 16/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SchwiftyApp/SchwiftyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchwiftyAppApp.swift
+//  SchwiftyApp
+//
+//  Created by Filip Krawczyk on 16/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct SchwiftyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
